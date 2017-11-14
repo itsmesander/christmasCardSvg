2f4034a84afc848f5a076c2b12f0fc3bab99a31e
Vik (Laptop) <vik.bessems@outlook.com>
2017-11-14 15:51:07 +0100

    Pattern code and css changes
    Pattern code + css changes

--- script.js
+++ script.js
@@ -8,18 +8,22 @@ function eventWindowLoaded() {
 }
 
 function initCanvas() {
-    canvas = document.getElementById("myCanvas");
-    if (!theCanvas || !isCanvasSupported) {
-        alert("broken");
+    canvas = document.getElementById("mycanvas");
+    if (!canvas || !isCanvasSupported) {
+        alert("404 Ondersteundt geen canvas");
         return;
     }
 
     ctx = canvas.getContext('2d');
-    //Start the application
-    startTheApp();
+    resizeCanvas();
 }
 
 
+function resizeCanvas(){
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    startTheApp();
+}
 
 function startTheApp() {
     var cnvsWidth = canvas.width;
@@ -29,12 +33,36 @@ function startTheApp() {
     ctx.fillRect(0,0, cnvsWidth, cnvsHeight);
     ctx.lineWidth = 1;
     
-    var tint = rondaf(0,360);
-    var zijde = 20;
-    var halfZijde = zijde / 2;
-    var rijHoogte = Math.floor(zijde * hScale);
-    var kolomen = Math.ceil(cnvsWidth / zijde) + 1;
-    var rijen = Math.ceil(cnvsHeight / rijHoogte);
+    var hueBegin = rondaf(0,360);
+    var triangleSide = 20;
+    var halfTriSide = triangleSide / 2;
+    var rowHeight = Math.floor(triangleSide * hScale);
+    var colums = Math.ceil(cnvsWidth / triangleSide) + 1;
+    var rows = Math.ceil(cnvsHeight / rowHeight);
+    
+    var col, row;
+    for(row = 0; row < rows; row++){
+        var hue = hueBegin + (row * 3);
+        for (col = 0; col < colums; col++){
+            var x = col * triangleSide;
+            var y = row * rowHeight
+            var color;
+            if(row % 2 != 0){
+                x -= halfTriSide;
+            }
+            //Driehoeken met punt naar boven
+            color = "hsl(" + hue + ", 50%, " + rondaf(0, 60) + "%)";
+            ctx.fillStyle = color;
+            ctx.strokeStyle = color;
+            ctx.beginPath();
+            ctx.moveTo(x,y);
+            ctx.lineTo(x + triangleSide, y);
+            ctx.lineTo(x + halfTriSide, y + rowHeight);
+            ctx.closePath();
+            ctx.fill();
+            ctx.stroke();
+        }
+    }
     
 }
 
@@ -46,4 +74,11 @@ function isCanvasSupported() {
 //afronden
 function rondaf(min, max){
     return Math.floor((Math.random() * (max - min + 1)) + min);
+}
+
+
+document.body.onkeyup = function(e){
+    if(e.keyCode == 32){
+        startTheApp();
+    }
 }
